f5f25066ec7dfa7bd2c2f67e99fac8622cad239c
AdamCzuba <89517332+AdamCzuba@users.noreply.github.com>
2022-05-10 16:07:13 +0100

    remote

--- src/components/Home/homecontent/HomeAboutUs.js
+++ src/components/Home/homecontent/HomeAboutUs.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function HomeAboutUs() {
-  return <div> hop hop</div>;
+  return (
+    <section className="aboutUs_section">
+      <div className="aboutUs_container">
+        <div className="aboutUS_wrapper">
+          <div className="primary_title">O nas</div>
+          <div className="decoration"></div>
+        </div>
+      </div>
+      <div className="aboutUs_img"></div>
+
+    </section>
+  )
 }
