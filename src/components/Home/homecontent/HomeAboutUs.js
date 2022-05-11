ddb6dc5d47428d32db8cf96f8623397aae239aea
AdamCzuba <89517332+AdamCzuba@users.noreply.github.com>
2022-05-11 15:01:00 +0100

    remote

--- src/components/Home/homecontent/HomeAboutUs.js
+++ src/components/Home/homecontent/HomeAboutUs.js
@@ -7,6 +7,7 @@ export default function HomeAboutUs() {
         <div className="aboutUS_wrapper">
           <div className="primary_title">O nas</div>
           <div className="decoration"></div>
+          <div className="primary_title">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</div>
         </div>
       </div>
       <div className="aboutUs_img"></div>
